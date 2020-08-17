d401386b08e60e5a0e47b28a486a717299ab23c3
rayhanbepary <ceorayhan2000@gmail.com>
2020-08-17 20:17:35 +0600

    hard rock

---
+++ rock.js
@@ -0,0 +1,54 @@
+const searchedSong          = document.getElementById('searchedSong');
+const searchBtn             = document.getElementById('searchBtn');
+const artistName            = document.getElementById('artistName');
+const title                 = document.getElementById('songTitle');
+const lyrics                = document.getElementById('lyrics');
+const searchResultContainer = document.getElementById('searchResultContainer');
+
+const Base_URL ='https://api.lyrics.ovh';
+
+searchBtn.addEventListener('click', () => {
+    fetch(''+Base_URL+'/suggest/'+searchedSong.value+'')
+    .then(response => response.json())
+    .then(data => {
+        let song = data.data;
+        song.slice(0,10).map(res => {
+            searchResultContainer.innerHTML += `
+                <div class="single-result row align-items-center my-3 p-3">
+                    <div class="col-md-9">
+                        <h3 class="lyrics-name">${res.title}</h3>
+                        <p class="author lead">Album by <span>${res.artist.name}</span></p>
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button class="btn btn-success" data-artist="${res.artist.name}" data-songtitle="${res.title}">Get Lyrics</button>
+                    </div>
+                </div>`
+            });
+    })
+    .catch( error => console.log(error))
+});
+
+//get lyrics
+getLyrics = (artist,songTitle) => {
+    fetch(''+Base_URL+'/v1/'+artist+'/'+songTitle+'/')
+    .then( res => res.json())
+    .then( data => {
+        lyrics.innerHTML     = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+        artistName.innerHTML = artist + " - ";
+        title.innerHTML      = songTitle;
+    })
+    .catch(error => console.log(error));
+}
+searchResultContainer.addEventListener('click', event => {
+    const clickedElement = event.target;
+    if(clickedElement.tagName === "BUTTON"){
+        const artist = clickedElement.getAttribute('data-artist');
+        const title = clickedElement .getAttribute('data-songtitle');
+
+        getLyrics(artist,title);
+    }
+});
+
+
+
+
