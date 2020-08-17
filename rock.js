c6a5d2bcc53a9658439db95c9929bc781395bae3
rayhanbepary <ceorayhan2000@gmail.com>
2020-08-17 23:04:18 +0600

    hard rock

--- rock.js
+++ rock.js
@@ -1,20 +1,26 @@
+const form = document.getElementById('form');
 const searchedSong          = document.getElementById('searchedSong');
-const searchBtn             = document.getElementById('searchBtn');
 const artistName            = document.getElementById('artistName');
 const title                 = document.getElementById('songTitle');
 const lyrics                = document.getElementById('lyrics');
 const searchResultContainer = document.getElementById('searchResultContainer');
 
+//lyrics API
 const Base_URL ='https://api.lyrics.ovh';
 
-searchBtn.addEventListener('click', () => {
-    fetch(''+Base_URL+'/suggest/'+searchedSong.value+'')
+searchSongs = () => {
+    fetch(`${Base_URL}/suggest/${searchedSong.value}`)
     .then(response => response.json())
     .then(data => {
-        let song = data.data;
-        song.slice(0,10).map(res => {
-            searchResultContainer.innerHTML += `
-                <div class="single-result row align-items-center my-3 p-3">
+        singleSong(data);
+    })
+    .catch( error => console.log(error))
+};
+
+function singleSong(data){
+    let song = data.data;
+    searchResultContainer.innerHTML = song.slice(0,10).map( res => {    
+        return   `<div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9">
                         <h3 class="lyrics-name">${res.title}</h3>
                         <p class="author lead">Album by <span>${res.artist.name}</span></p>
@@ -23,10 +29,8 @@ searchBtn.addEventListener('click', () => {
                         <button class="btn btn-success" data-artist="${res.artist.name}" data-songtitle="${res.title}">Get Lyrics</button>
                     </div>
                 </div>`
-            });
-    })
-    .catch( error => console.log(error))
-});
+    }).join('')
+}
 
 //get lyrics
 getLyrics = (artist,songTitle) => {
@@ -39,15 +43,27 @@ getLyrics = (artist,songTitle) => {
     })
     .catch(error => console.log(error));
 }
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    searchSongs();
+    lyrics.innerHTML     = "";
+    artistName.innerHTML = "";
+    title.innerHTML      = "";
+});
+
+
 searchResultContainer.addEventListener('click', event => {
     const clickedElement = event.target;
     if(clickedElement.tagName === "BUTTON"){
         const artist = clickedElement.getAttribute('data-artist');
         const title = clickedElement .getAttribute('data-songtitle');
-
+        
         getLyrics(artist,title);
     }
 });
+
+
 
 
 
